490bf7f0b8122ef04c84141ac3d7d8bbb7e46ac3
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-12-07 22:42:56 +0200

    Add part 2 for day 2.

--- day7/whale.cpp
+++ day7/whale.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 
-#if 1
+#if 0
 const char *filename = "sample.txt";
 const int POS_SZ = 12;
 #else
@@ -64,10 +64,49 @@ void part1()
 
     std::cout << "Part 1: " << min_score << '\n';
 }
+void part2()
+{
+    int positions[POS_SZ] = {0};
+    size_t pos_size = 0;
+
+    std::ifstream fin(filename);
+    if (!fin)
+    {
+        std::cout << "ERROR: Could not open " << filename << '\n';
+        return;
+    }
+
+    std::string buffer;
+    std::getline(fin, buffer);
+
+    parse_numbers(buffer, positions, pos_size);
+
+    int min_score = -1;
+
+    for (int position = 0; position < pos_size; ++position)
+    {
+        int current_score = 0;
+
+        for (int i = 0; i < pos_size; ++i)
+        {
+            int distance = abs(positions[i] - position);
+            current_score += distance * (distance + 1) / 2;
+        }
+
+        if (min_score < 0 || current_score < min_score)
+        {
+            min_score = current_score;
+        }
+    }
+
+    std::cout << "Part 2: " << min_score << '\n';
+}
+
 
 int main(int argc, char const *argv[])
 {
     part1();
+    part2();
 
     return 0;
 }
