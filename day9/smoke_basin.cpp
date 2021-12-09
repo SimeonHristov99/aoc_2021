62402a48cc8bc1856340a28c7aa3cdb90d60011b
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-12-09 22:57:21 +0200

    Refactoring.

--- day9/smoke_basin.cpp
+++ day9/smoke_basin.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 
-#if 0
+#if 1
 const char *filename = "sample.txt";
 const int MAP_SIZE = 10;
 #else
@@ -21,7 +21,31 @@ void dump_map(const int map[][MAP_SIZE], int w, int h)
     }
 }
 
-void get_risk(const int map[][MAP_SIZE], int w, int h)
+void parse(int map[][MAP_SIZE], int *h, int *w)
+{
+    std::ifstream fin(filename);
+    if (!fin)
+    {
+        std::cout << "ERROR: Could not open " << filename << '\n';
+        return;
+    }
+
+    std::string buffer;
+    fin >> buffer;
+    *w = buffer.length();
+
+    do
+    {
+        for (int i = 0; i < *w; ++i)
+        {
+            map[*h][i] = buffer[i] - '0';
+        }
+
+        ++*h;
+    } while (fin >> buffer);
+}
+
+int part1(int map[][MAP_SIZE], int w, int h)
 {
     int result = 0;
 
@@ -33,51 +57,27 @@ void get_risk(const int map[][MAP_SIZE], int w, int h)
                 i - 1 >= 0 && map[i][j] >= map[i - 1][j]
                 || i + 1 < h && map[i][j] >= map[i + 1][j]
                 || j - 1 >= 0 && map[i][j] >= map[i][j - 1]
-                || j + 1 < w && map[i][j] >= map[i][j + 1]
-            ))
+                || j + 1 < w && map[i][j] >= map[i][j + 1])
+            )
             {
                 result += map[i][j] + 1;
             }
         }
     }
 
-    std::cout << result << '\n';
-}
-
-void part1()
-{
-    int map[MAP_SIZE][MAP_SIZE];
-    int h = 0, w = 0;
-
-    std::ifstream fin(filename);
-    if (!fin)
-    {
-        std::cout << "ERROR: Could not open " << filename << '\n';
-        return;
-    }
-
-    std::string buffer;
-    fin >> buffer;
-    w = buffer.length();
-
-    do
-    {
-        for (int i = 0; i < w; ++i)
-        {
-            map[h][i] = buffer[i] - '0';
-        }
-
-        ++h;
-    } while (fin >> buffer);
-
-    // dump_map(map, w, h);
-
-    get_risk(map, w, h);
+    return result;
 }
 
 int main(int argc, char const *argv[])
 {
-    part1();
+    int map[MAP_SIZE][MAP_SIZE];
+    int h = 0, w = 0;
+
+    parse(map, &h, &w);
+
+    dump_map(map, w, h);
+
+    std::cout << "Part 1: " << part1(map, w, h) << '\n';
 
     return 0;
 }
