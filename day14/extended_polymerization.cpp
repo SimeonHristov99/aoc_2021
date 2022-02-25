b88d83b437509ad30d86d1c1fa7aaa1c0afc573a
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-02-25 12:25:05 +0200

    Move counting to another array.

--- day14/extended_polymerization.cpp
+++ day14/extended_polymerization.cpp
@@ -6,7 +6,22 @@ const char *FILENAME = "sample.txt";
 const int SIZE = 26;
 const int STEPS = 1;
 
-void parse(std::vector<std::string> &subs, int grid[][SIZE][2])
+void init_grid(int grid[][SIZE])
+{
+    for (int i = 0; i < SIZE; ++i)
+    {
+        for (int j = 0; j < SIZE; ++j)
+        {
+            grid[i][j] = 0;
+        }
+    }
+}
+
+void parse(
+    std::vector<std::string> &subs,
+    int grid[][SIZE],
+    int occurs[SIZE]
+)
 {
     std::string buffer, sub;
     std::ifstream fin(FILENAME);
@@ -22,9 +37,14 @@ void parse(std::vector<std::string> &subs, int grid[][SIZE][2])
     int len_buf = buffer.length();
     for (int i = 0; i < len_buf - 1; i++)
     {
-        ++grid[buffer[i] - 'A'][buffer[i + 1] - 'A'][0];
-        grid[buffer[i] - 'A'][buffer[i + 1] - 'A'][1] = true;
+        int from = buffer[i] - 'A';
+        int to = buffer[i + 1] - 'A';
+        
+        ++grid[from][to];
+        
+        ++occurs[from];
     }
+    ++occurs[buffer[len_buf - 1] - 'A'];
 
     std::getline(fin, buffer);
 
@@ -41,54 +61,73 @@ void parse(std::vector<std::string> &subs, int grid[][SIZE][2])
     fin.close();
 }
 
-void init_grid(int grid[][SIZE][2])
+void print_grid(int grid[][SIZE])
 {
     for (int i = 0; i < SIZE; ++i)
     {
         for (int j = 0; j < SIZE; ++j)
         {
-            grid[i][j][0] = 0;
-            grid[i][j][1] = 0;
-        }
-    }
-}
-
-void print_grid(int grid[][SIZE][2])
-{
-    for (int i = 0; i < SIZE; ++i)
-    {
-        for (int j = 0; j < SIZE; ++j)
-        {
-            std::cout << '[' << grid[i][j][0] << ' ' << grid[i][j][1] << "] ";
+            std::cout << grid[i][j] << ' ';
         }
         std::cout << '\n';
     }
 }
 
+void print_occurs(int occurs[SIZE])
+{
+    for (int i = 0; i < SIZE; ++i)
+    {
+        std::cout << occurs[i] << ' ';
+    }
+    std::cout << '\n';
+}
+
 int main(int argc, char const *argv[])
 {
     std::vector<std::string> subs;
-    int grid[SIZE][SIZE][2];
+    int occurs[SIZE] = { 0 };
+    int grid[SIZE][SIZE];
 
     init_grid(grid);
-    parse(subs, grid);
+    parse(subs, grid, occurs);
 
     for (int step = 0; step < STEPS; ++step)
     {
+        std::vector<std::string> matched;
+
+        // consider all pairs "simultaneously"
         for (const std::string &sub : subs)
         {
-            if (grid[sub[0] - 'A'][sub[1] - 'A'][1])
+            if (grid[sub[0] - 'A'][sub[1] - 'A'] > 0)
             {
-                std::cout << "match for " << sub[0] << sub[1] << '\n';
-                grid[sub[0] - 'A'][sub[1] - 'A'][1] = false;
-
-                ++grid[sub[0] - 'A'][sub[2] - 'A'][0];
-                grid[sub[0] - 'A'][sub[2] - 'A'][1] = true;
+                matched.push_back(sub);
             }
+        }
+
+        for (const std::string &match : matched)
+        {
+            // insert new letter
+            // NN, A
+            // NAN
+            ++grid[match[0] - 'A'][match[2] - 'A'];
+            ++grid[match[2] - 'A'][match[1] - 'A'];
+
+            // increment the occurences of the new element
+            // grid[match[0] - 'A'][match[1] - 'A'] is the 
+            // number of times we have this pair. We have to
+            // insert the new letter for each of these pairs
+            // occurs[match[2] - 'A'] += (occurs[match[2] - 'A'] == 0 ? 1 : 0);
+            occurs[match[2] - 'A'] += grid[match[0] - 'A'][match[1] - 'A'];
+
+            // disconnect pair
+            grid[match[0] - 'A'][match[1] - 'A'] = 0;
         }
     }
 
     print_grid(grid);
+    std::cout << "\nOccurs: ";
+    print_occurs(occurs);
+
 
     return 0;
 }
