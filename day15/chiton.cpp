bf50d5f8a6ffe1fa9d8110ce1539762aba16db07
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-03-09 14:26:13 +0200

    Try to impl dijkstra.

--- day15/chiton.cpp
+++ day15/chiton.cpp
@@ -15,15 +15,17 @@ const int GRID_CAP = 128;
 
 struct Cell
 {
+    int back_i;
+    int back_j;
     int i;
     int j;
-    int gn;
+    int gn = 10000;
+    int risk;
 };
 
 std::pair<int, int> NEIGHBOURS[4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-inline size_t key(int i,int j) {return (size_t) i << 32 | (unsigned int) j;}
 
-void parse(int grid[][GRID_CAP], int &grid_sz)
+void parse(Cell grid[][GRID_CAP], int &grid_sz)
 {
     int i = 0, j = 0;
     std::string buffer;
@@ -39,8 +41,16 @@ void parse(int grid[][GRID_CAP], int &grid_sz)
     {
         for (const char c : buffer)
         {
-            grid[i][j++] = c - '0';
+            Cell cell;
+            cell.i = -1;
+            cell.j = -1;
+            cell.back_i = -1;
+            cell.back_j = -1;
+            cell.risk = c - '0';
+
+            grid[i][j++] = cell;
         }
+
         ++i;
         j = 0;
     }
@@ -50,93 +60,85 @@ void parse(int grid[][GRID_CAP], int &grid_sz)
     grid_sz = i;
 }
 
-void dump_grid(int grid[][GRID_CAP], int grid_sz)
+void dump_grid(Cell grid[][GRID_CAP], int grid_sz)
 {
     for (int i = 0; i < grid_sz; ++i)
     {
         for (int j = 0; j < grid_sz; ++j)
         {
-            std::cout << grid[i][j] << ' ';
+            std::cout << grid[i][j].j << ' ';
         }
         std::cout << '\n';
     }
 }
 
-// bool not_elem(const std::vector<Cell> &p, int i, int j)
-// {
-//     for (const Cell &c : p)
-//     {
-//         if (c.first == i && c.second == j)
-//         {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-int euclidean_distance(const Cell &cell, int sz)
+void dijkstra(Cell grid[][GRID_CAP], int grid_sz)
 {
-    return std::sqrt((cell.i - sz) * (cell.i - sz) + (cell.j - sz) * (cell.j - sz));
-}
-
-void bfs(int grid[][GRID_CAP], int grid_sz)
-{
-    auto cmp = [grid_sz](const Cell &p1, const Cell &p2)
+    auto cmp = [](const Cell &c1, const Cell &c2)
     {
-        return p1.gn < p2.gn;
+        return c1.gn > c2.gn;
     };
 
+    int goal_x = grid_sz - 1;
+    int goal_y = grid_sz - 1;
     std::priority_queue<Cell, std::vector<Cell>, decltype(cmp)> frontier(cmp);
-    std::unordered_map<size_t, int> visited;
-    frontier.push({0, 0, 0});
+    grid[0][0].i = 0;
+    grid[0][0].j = 0;
+    grid[0][0].back_i = 0;
+    grid[0][0].back_j = 0;
+    grid[0][0].gn = 0;
+    grid[0][0].risk = 0;
+    frontier.push(grid[0][0]);
 
     while (!frontier.empty())
     {
         Cell current_cell = frontier.top();
         frontier.pop();
 
-        if (current_cell.i == grid_sz - 1 && current_cell.j == grid_sz - 1)
+        // std::cout << current_cell.gn << '\n';
+
+        if (current_cell.i == goal_x && current_cell.j == goal_y)
         {
             std::cout << current_cell.gn << '\n';
             return;
         }
 
-        for(const std::pair<int, int> &neigh : NEIGHBOURS)
+        for (const std::pair<int, int> &neigh : NEIGHBOURS)
         {
-            Cell next;
-            next.i = current_cell.i + neigh.first;
-            next.j = current_cell.j + neigh.second;
+            int next_i = current_cell.i + neigh.first;
+            int next_j = current_cell.j + neigh.second;
 
-            if(next.i < 0 || next.j >= grid_sz)
+            if (next_i < 0 || next_i >= grid_sz || next_j < 0 || next_j >= grid_sz)
             {
                 continue;
             }
 
-            next.gn = current_cell.gn + grid[next.i][next.j];
-            std::unordered_map<size_t, int>::iterator seen_coords = visited.find(key(next.i, next.j));
+            Cell next = grid[next_i][next_j];
 
-            if(seen_coords != visited.end())
+            if (next.back_i == -1 && next.back_j == -1 || current_cell.gn + next.risk < next.gn)
             {
-                if (seen_coords->second < next.gn)
-                {
-                    frontier.push({next.i, next.j});
-                }
+                next.back_i = current_cell.i;
+                next.back_j = current_cell.j;
+                next.i = next_i;
+                next.j = next_j;
+                next.gn = current_cell.gn + next.risk;
+                frontier.push(next);
             }
-
-            
         }
     }
+
+    std::cout << "Frontier empty :(\n";
 }
 
 void part1()
 {
     // parse the grid
     int grid_sz = 0;
-    int grid[GRID_CAP][GRID_CAP];
+    Cell grid[GRID_CAP][GRID_CAP];
     parse(grid, grid_sz);
-    dump_grid(grid, grid_sz);
+    // dump_grid(grid, grid_sz);
 
-    // bfs(grid, grid_sz);
+    dijkstra(grid, grid_sz);
 }
 
 int main(int argc, char const *argv[])
