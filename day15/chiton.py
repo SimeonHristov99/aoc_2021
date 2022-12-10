637908ae9b0f1a8baaff0b768a3a87ef482001f0
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-12-10 19:56:09 +0200

    Mange to solve it. Solution was to use a simplified version of the  Dijkstra algorithm.

--- day15/chiton.py
+++ day15/chiton.py
@@ -1,121 +1,86 @@
 import numpy as np
-from queue import PriorityQueue
-import functools
+from queue import Queue
 
+NEIGHBORS = {
+    (-1, 0),
+    (0, -1),
+    (1, 0),
+    (0, 1),
+}
 
-@functools.total_ordering
-class Node:
-    def __init__(self, x, y, total_risk):
-        self.x = x
-        self.y = y
-        self.total_risk = total_risk
-
-    def __lt__(self, other):
-        return self.total_risk < other.total_risk
-
-    def __eq__(self, other):
-        return self.total_risk == other.total_risk
-
-
-def is_valid(coord, upper_limit):
-    return 0 <= coord[0] < upper_limit and 0 <= coord[1] < upper_limit
-
-
-def bfs(matrix, start, end):
-    q = PriorityQueue()
-    q.put(Node(start[0], start[1], 0))
-    visited = set()
-    upper_limit = matrix.shape[0]
-
-    while not q.empty():
-        current_node = q.get()
-        x, y, tot_risk = current_node.x, current_node.y, current_node.total_risk
-        visited.add((x, y))
-
-        if (x, y) == end:
-            return current_node
-
-        candid = (x-1, y)
-        if candid not in visited and is_valid(candid, upper_limit):
-            # left
-            q.put(Node(*candid, tot_risk + matrix[candid]))
-            visited.add((x, y))
-
-        candid = (x, y-1)
-        if candid not in visited and is_valid(candid, upper_limit):
-            # up
-            q.put(Node(*candid, tot_risk + matrix[candid]))
-            visited.add((x, y))
-
-        candid = (x+1, y)
-        if candid not in visited and is_valid(candid, upper_limit):
-            # right
-            q.put(Node(*candid, tot_risk + matrix[candid]))
-            visited.add((x, y))
-
-        candid = (x, y+1)
-        if candid not in visited and is_valid(candid, upper_limit):
-            # down
-            q.put(Node(*candid, tot_risk + matrix[candid]))
-            visited.add((x, y))
-
-    return -1
-
-
-def dp(matrix):
-    table = np.zeros_like(matrix)
-
-    size = matrix.shape[0]
-
-    for j in range(1, size):
-        table[0, j] = table[0, j - 1] + matrix[0, j]
-
-    for i in range(1, size):
-        table[i, 0] = table[i - 1, 0] + matrix[i, 0]
-
-    for i in range(1, size):
-        for j in range(1, size):
-            table[i, j] = min(table[i - 1, j], table[i, j - 1]) + matrix[i, j]
-
-    print(table)
-
-    return table[size - 1, size - 1]
+BIG_SIZE = 5
 
 
 def part1(file: str):
+    q = Queue()
+
     cave = np.genfromtxt(file, delimiter=1, dtype=int)
-    return dp(cave)
+    init_s = cave.shape[0]
+    size = cave.shape[0]
+
+    table_dists = np.zeros((size, size)) + np.inf
+    table_dists[0, 0] = 0
+
+    q.put((0, 0))
+
+    while not q.empty():
+        (x, y) = q.get()
+
+        for (dx, dy) in NEIGHBORS:
+            i = x + dx
+            j = y + dy
+
+            risk = get_big_map_val(
+                cave[i % init_s, j % init_s], i // init_s, j // init_s)
+
+            if 0 <= i < size and 0 <= j < size \
+                    and table_dists[x, y] + risk < table_dists[i, j]:
+                table_dists[i, j] = table_dists[x, y] + risk
+                q.put((i, j))
+
+    return table_dists[-1, -1].astype(int)
+
+
+def get_big_map_val(n, i, j):
+    return ((n - 1 + j) % 9 + i) % 9 + 1
 
 
 def part2(file: str):
-    cave0 = np.genfromtxt(file, delimiter=1, dtype=int)
+    q = Queue()
 
-    caves = np.tile(cave0, (5, 5, 1)).reshape(5, 5, *cave0.shape)
+    cave = np.genfromtxt(file, delimiter=1, dtype=int)
+    init_s = cave.shape[0]
+    size = cave.shape[0] * BIG_SIZE
 
-    for i in range(5):
-        for j in range(5):
-            if i == 0 and j == 0:
-                continue
+    table_dists = np.zeros((size, size)) + np.inf
+    table_dists[0, 0] = 0
 
-            if i == 0:
-                caves[i, j] = caves[i, j - 1] + 1
-            else:
-                caves[i, j] = caves[i - 1, j] + 1
+    q.put((0, 0))
 
-            caves[i, j] = np.where(caves[i, j] > 9, 1, caves[i, j])
+    while not q.empty():
+        (x, y) = q.get()
 
-    caves = np.concatenate(caves, -2)
-    caves = np.concatenate(caves, -1)
+        for (dx, dy) in NEIGHBORS:
+            i = x + dx
+            j = y + dy
 
-    return dp(caves)
+            risk = get_big_map_val(
+                cave[i % init_s, j % init_s], i // init_s, j // init_s)
+
+            if 0 <= i < size and 0 <= j < size \
+                    and table_dists[x, y] + risk < table_dists[i, j]:
+                table_dists[i, j] = table_dists[x, y] + risk
+                q.put((i, j))
+
+    return table_dists[-1, -1].astype(int)
 
 
 def main():
-    # print(f"Part 1 (sample): {part1('./sample.txt')}")
+    print(f"Part 1 (sample): {part1('./sample.txt')}")
     print(f"Part 1 (input): {part1('./input.txt')}")
-
+    print()
     print(f"Part 2 (sample): {part2('./sample.txt')}")
-    # print(f"Part 2 (input): {part2('./input.txt')}")
+    print(f"Part 2 (input): {part2('./input.txt')}")
 
 
 if __name__ == '__main__':
